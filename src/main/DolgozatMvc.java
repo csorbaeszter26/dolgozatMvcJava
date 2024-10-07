b94eed518865cd95e1ff86f7e75f6abcb00a482a
csorbaeszter26 <csorba.eszter.26@gmail.com>
2024-10-07 13:04:08 +0200

    1

---
+++ src/main/DolgozatMvc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author csorba.e.katalin
+ */
+public class DolgozatMvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
